1ed32c350d2029b92fc1d03e073398059aa89d5e
Denis Romanenko <deromanenko@gmail.com>
2023-01-14 12:40:35 +0300

    Improved docker configuration for req installation

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
-assets:
-	docker compose run --rm node npm run dev
+install:
+	docker compose \
+		&& docker compose run --rm composer composer install \
+		&& docker compose run --rm node npm install
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,10 +7,23 @@
     @vite('resources/css/app.css')
 </head>
 
-<body class="bg-slate-500">
-    <h1 class="text-3xl font-bold underline text-white">
-        Hello world!
-    </h1>
+<body>
+    <div class="flex justify-center">
+        <!-- Main Y Container -->
+        <div class="w-full md:w-3/4 xl:w-3/4">
+            <nav class="flex items-center justify-between p-5">
+                <!-- Logo -->
+                <div>
+                    <img src="/images/phormally_logo.png" class="w-[15rem]" alt="">
+                </div>
+                <!-- Buttons -->
+                <div class="">
+                    <button>Log In</button>
+                </div>
+            </nav>
+        </div>
+    </div>
+
 </body>
 
 </html>
