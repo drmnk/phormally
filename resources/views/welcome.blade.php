90288fcc58b48a034a120fe0d19998be574093c8
Denis Romanenko <deromanenko@gmail.com>
2023-01-15 22:36:05 +0300

    Finish first version of Welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -57,13 +57,16 @@
 
     <section>
         <div class="flex flex-col md:flex-row md:space-x-10 md:w-3/4 lg:w-1/2 mx-auto">
-            <img class="max-w-[256px] mx-auto"src="/images/tasks_in_notebook.png" alt="">
+            <div class="">
+                <img class="max-w-[256px] mx-auto"src="/images/tasks_in_notebook.png" alt="">
+            </div>
             <div class="p-6">
-                <h1 class="text-3xl">You've created a beatiful website</h1>
-                <p class="text-lg font-light mt-4">It can be a landing page, your online "business card" or simple
+                <span class="text-3xl text-white bg-pink-600">You've created a beatiful website</span>
+                <p class="text-lg font-light mt-4">HTML/CSS, not much but it's honest work. It can be a landing page,
+                    your online "business card" or simple
                     blog. Just three tasks left in your notebook:</p>
                 <ul class="list-disc ml-4 mt-4 text-lg">
-                    <li>Integrate form for your website visitors feedback</li>
+                    <li class="text-pink-600 italic">Integrate form for your website visitors feedback - ASAP!</li>
                     <li>Deploy this beatiful thing to Netlify or Vercel</li>
                     <li>Celebrate it with a bottle of Crystal</li>
                 </ul>
@@ -72,13 +75,83 @@
         </div>
     </section>
 
+    <section class="bg-slate-100 py-6">
+        <div class="flex flex-col md:flex-row md:space-x-10 md:w-3/4 lg:w-1/2 mx-auto mt-10 translate-x-6">
+            <div class="p-6">
+                <span class="text-3xl text-white bg-blue-600">I need a backend? Not cool!</span>
+                <p class="text-lg font-light mt-4">It's a shame, but to process user input even in the simplest form
+                    ever - you need some server code. So, what you gonna do next?</p>
+                <ul class="list-disc ml-4 mt-4 text-lg">
+                    <li>Call your fellow programmer who knows some Java or C#</li>
+                    <li>Learn NodeJS or Python in a hurry</li>
+                    <li>Try to put your beautiful design in a Wordpress with sendmail plugins</li>
+                </ul>
+                </p>
+            </div>
+            <div>
+                <img class="transform scale-x-flip max-w-[256px] mx-auto"src="/images/tech_frustration.png"
+                    alt="">
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="flex flex-col md:flex-row md:space-x-10 md:w-3/4 lg:w-1/2 mx-auto mt-10">
+            <div class="">
+                <img class="max-w-[256px] mx-auto"src="/images/getmail.png" alt="">
+            </div>
+
+            <div class="p-6">
+                <span class="text-3xl text-black">Meet the Phormally</span>
+                <p class="text-slate-600 text-sm font-extralight"> "- Hello Phormally!"</p>
+                <p class="text-lg font-light mt-4">All you need to do is to put one line of code in your form. Phormally
+                    will take care of the rest:</p>
+                <ul class="list-disc ml-4 mt-4 text-lg">
+                    <li>Send you all form submissions by the mail</li>
+                    <li>Save it online for easy browsing</li>
+                    <li>Export data in CSV format</li>
+                    <li class="text-pink-600 italic">More features in the future!</li>
+                </ul>
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <section class="mt-10 flex flex-col">
+        <div class="bg-white rounded-xl shadow-2xl mx-auto p-20">
+            <h1 class="mt-5 text-4xl text-center">It's fully free. What are you waiting for?</h1>
+            <div class="flex mt-6 justify-center space-x-6">
+                <div>
+                    <img src="/images/phormally_logo.png" class="w-[15rem]" alt="">
+                </div>
+                <div>
+                    <button
+                        class="border text-xs md:text-xl bg-pink-500 text-white rounded-xl px-6 py-4 transition duration-500 select-none hover:bg-pink-600 shadow-md mt-[0.4rem]">
+                        Start using
+                    </button>
+                </div>
+            </div>
+        </div>
+    </section>
 
 
-    Illustration by <a href="https://icons8.com/illustrations/author/lZpGtGw5182N">Elisabet Guba</a> from <a
-        href="https://icons8.com/illustrations">Ouch!</a>
+    <footer class="mt-40  bg-slate-300 h-40 p-10">
+        <div class="">
+            Illustration by <a href="https://icons8.com/illustrations/author/lZpGtGw5182N">Elisabet Guba</a> from <a
+                href="https://icons8.com/illustrations">Ouch!</a>
+        </div>
+        <div>
+            Illustration by <a href="https://icons8.com/illustrations/author/iAdLsFJOKDrk">Tanya Krasutska</a> from <a
+                href="https://icons8.com/illustrations">Ouch!</a>
+        </div>
+        <div class="mt-6">
+            Copyright © 2023. <a class="font-bold" href="https://github.com/drmnk/phormally">Github: drmnk/phormally</a>
+        </div>
 
-    Illustration by <a href="https://icons8.com/illustrations/author/iAdLsFJOKDrk">Tanya Krasutska</a> from <a
-        href="https://icons8.com/illustrations">Ouch!</a>
+
+
+    </footer>
+
 </body>
 
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
     extend: {
       fontFamily: {
         'sans': ['Sofia Sans', ...defaultTheme.fontFamily.sans]
+      },
+      scale: {
+        'flip': '-1'
       }
     },
   },
